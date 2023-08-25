65cadf6d91fc4229071e43919ea0404c8aea7cfb
Jetnipat-c <alonecheer@hotmail.com>
2023-08-25 21:51:07 +0700

    add log

--- src/merge.ts
+++ src/merge.ts
@@ -20,4 +20,6 @@ const collection1 = [4, 1, 3, 5];
 const collection2 = [8, 2, 7, 9, 6];
 
 const result = merge(collection1, collection2);
-console.log(result);
+console.log("collection1: ", collection1);
+console.log("collection2: ", collection2);
+console.log("result: ", result);
